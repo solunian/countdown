c27a5e1aa594f952299c734262cbed3acc59ffd4
Daniel Cui <solunian@gmail.com>
2024-09-27 09:48:57 -0700

    chore: scaffold html/css nonsense

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1 class="text-5xl text-blue-700">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex h-screen w-screen justify-center bg-gray-950">
+  <main
+    class="shadow-glow m-auto h-1/2 w-4/5 rounded-xl border-none bg-gray-50 p-4 shadow-gray-50 sm:w-3/4 md:w-2/3 lg:w-1/2">
+    hello!
+  </main>
+</div>
+
+<style>
+  .shadow-glow {
+    box-shadow:
+      0 0 20px 0 #f9fafb,
+      0 0 20px 0 #f9fafb;
+  }
+</style>
